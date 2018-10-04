64ec0f3e26e5854a0d4f1b71d31eef2ed636b42d
Manila-Kristin <manilakristin21@gmail.com>
2018-10-04 13:03:31 +0700

    test technical

---
+++ application/views/dashboard/index.php
@@ -0,0 +1,219 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Vertilogic</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/style.css")?>">
+
+</head>
+<body>
+  <style>
+    .google-maps {
+        position: relative;
+        padding-bottom: 75%; // This is the aspect ratio
+        height: 0;
+        overflow: hidden;
+    }
+    .google-maps iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100% !important;
+        height: 100% !important;
+    }
+</style>
+
+
+  
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">My Admin</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome Admin, Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+<div class="container">
+
+  <div class="main">
+    
+    <div class="left">
+      
+      <div class="well">
+        <img src="assets/img/user.png" class="img-circle" height="65" width="65" alt="Avatar"> Manila Kristin
+      </div>
+      <div class="well">
+        <h3 align="center"><strong><a href="#">Menu</a></strong></h3>
+        <p><a href="#">Homepage</a></p>
+        <p><a href="#">About</a></p>
+        <p><a href="#">Contact</a></p>
+      </div>
+    </div> <!--/ .header -->
+
+    <div class="middle">
+      <h3>All Users</h3>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="well">
+            
+            <div class="row">
+              <div class="col-md-2">
+                 <img src="assets/img/user.png" class="img-circle" height="55" width="55" alt="Avatar">
+              </div>
+              <div class="col-md-6">
+                  <p>Name: Leanne Graham</p>
+                  <p>Username: Bret</p>
+                  <a href="#"><span class="glyphicon glyphicon-envelope">&nbsp;</span>Sincere@april.biz</a>
+                  <p><span class="glyphicon glyphicon-earphone">&nbsp;</span>1-770-736-8031 x56442</p>
+                  <a href="#"><span class="glyphicon glyphicon-globe  ">&nbsp;</span>hildegard.org</a>
+                  <p>Company: Romaguera-Crona</p>
+                  <p>CatchPhrase: Multi-layered client-server neural-net</p>
+                  <p>Bs: harness real-time e-markets</p>
+              </div>
+              <div class="col-md-4">
+                  <div class="google-maps">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3173.1354242156517!2d81.14741131528768!3d-37.315619713475535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzfCsDE4JzU2LjMiUyA4McKwMDgnNTguNiJF!5e0!3m2!1sid!2sid!4v1538627212750" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>                     </div>
+                      <p><span class="glyphicon glyphicon-map-marker">&nbsp;</span>Kulas Light</p>   
+                      <p>Apt. 556,</p>   
+                      <p>Gwenborough</p> 
+                      <p>92998-3874</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="row">
+        <div class="col-md-12">
+          <div class="well">
+            
+            <div class="row">
+              <div class="col-md-2">
+                 <img src="assets/img/user.png" class="img-circle" height="55" width="55" alt="Avatar">
+              </div>
+              <div class="col-md-6">
+                  <p>Name: Ervin Howell</p>
+                  <p>Username: Antonette</p>
+                  <a href="#"><span class="glyphicon glyphicon-envelope">&nbsp;</span>Shanna@melissa.tv</a>
+                  <p><span class="glyphicon glyphicon-earphone">&nbsp;</span>010-692-6593 x09125</p>
+                  <a href="#"><span class="glyphicon glyphicon-globe  ">&nbsp;</span>anastasia.net</a>
+                  <p>Company: Deckow-Crist</p>
+                  <p>CatchPhrase: Proactive didactic contingency</p>
+                  <p>Bs: synergize scalable supply-chains</p>
+              </div>
+              <div class="col-md-4">
+                  <div class="google-maps">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2872.409354875083!2d-34.46398868447802!3d-43.950896117077235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDPCsDU3JzAzLjIiUyAzNMKwMjcnNDIuNSJX!5e0!3m2!1sid!2sid!4v1538625393419" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>                  </div>
+                      <p><span class="glyphicon glyphicon-map-marker">&nbsp;</span>Victor Plains</p>   
+                      <p>Suite 879,</p>   
+                      <p>Wisokyburgh</p> 
+                      <p>990566-7771</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
+
+      <div class="row">
+        <div class="col-md-12">
+          <div class="well">
+            
+            <div class="row">
+              <div class="col-md-2">
+                 <img src="assets/img/user.png" class="img-circle" height="55" width="55" alt="Avatar">
+              </div>
+              <div class="col-md-6">
+                  <p>Name: Clementine Bauch</p>
+                  <p>Username: Samantha</p>
+                  <a href="#"><span class="glyphicon glyphicon-envelope">&nbsp;</span>Nathan@yesenia.net</a>
+                  <p><span class="glyphicon glyphicon-earphone">&nbsp;</span>1-463-123-4447</p>
+                  <a href="#"><span class="glyphicon glyphicon-globe  ">&nbsp;</span>ramiro.info</a>
+                  <p>Company: Romaguera-Jacobson</p>
+                  <p>CatchPhrase: Face to face bifurcated interface</p>
+                  <p>Bs: e-enable strategic applications</p>
+              </div>
+              <div class="col-md-4">
+                  <div class="google-maps">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1455.1392358004975!2d-47.067488683410225!3d-68.61006902932922!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNjjCsDM2JzM2LjMiUyA0N8KwMDMnNTUuMSJX!5e0!3m2!1sid!2sid!4v1538627900827" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                  </div>
+                      <p><span class="glyphicon glyphicon-map-marker">&nbsp;</span>Douglas Extension</p>   
+                      <p>Suite 847,</p>   
+                      <p>McKenziehaven</p> 
+                      <p>59590-4157</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="row">
+        <div class="col-md-12">
+          <div class="well">
+            
+            <div class="row">
+              <div class="col-md-2">
+                 <img src="assets/img/user.png" class="img-circle" height="55" width="55" alt="Avatar">
+              </div>
+              <div class="col-md-6">
+                  <p>Name: Patricia Lebsack</p>
+                  <p>Username: Karianne</p>
+                  <a href="#"><span class="glyphicon glyphicon-envelope">&nbsp;</span>Julianne.OConner@kory.org</a>
+                  <p><span class="glyphicon glyphicon-earphone">&nbsp;</span>493-170-9623 x156</p>
+                  <a href="#"><span class="glyphicon glyphicon-globe  ">&nbsp;</span>kale.biz</a>
+                  <p>Company: Robel-Corkery</p>
+                  <p>CatchPhrase: Multi-tiered zero tolerance productivity</p>
+                  <p>Bs: transition cutting-edge web services</p>
+              </div>
+              <div class="col-md-4">
+                  <div class="google-maps">
+                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3474.0170432380705!2d-164.30118868515368!3d29.457512653014373!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjnCsDI3JzI3LjAiTiAxNjTCsDE3JzU2LjQiVw!5e0!3m2!1sid!2sid!4v1538628176269" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                   </div>
+                      <p><span class="glyphicon glyphicon-map-marker">&nbsp;</span>Hoeger Mall</p>   
+                      <p>Apt. 692,</p>   
+                      <p>South Elvis</p> 
+                      <p>53919-4257</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+       
+ 
+     </div> <!--/ .middle -->
+
+    <div class="right">
+      <h3 align="right">About Us</h3>
+      <p align="right">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+    </div> <!--/ .right -->
+
+  </div> <!--/ .main -->
+
+  <div class="footer">
+    <h4 align="center">Footer</h4>
+    <p align="center">Test <a href="#">Vertilogic.com</a></p>
+  </div> <!--/ .footer -->
+ 
+</div>
+
+</body>
+</html>
