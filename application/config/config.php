e83df6ed1633b60656d6d40d16ed29bc3e41fbe9
manilakristin <manilakristin21@gmail.com>
2018-10-04 13:21:31 +0700

    tes coding

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/tes_manila/';
+$config['base_url'] = 'http://localhost/manila/';
 
 /*
 |--------------------------------------------------------------------------
